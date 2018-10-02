09be41c0d8c014fc2a18471390b15b2f1d2069f5
Ali Nematbakhsh <anematba@nd.edu>
2018-10-02 14:29:29 -0400

    more uniform ECM nodes included in the input

--- src/srcGPU/SceECM.cu
+++ src/srcGPU/SceECM.cu
@@ -150,7 +150,7 @@ int numberNodes_ECM ;
 double tmpPosX_ECM,tmpPosY_ECM ; 
 vector<double> posXIni_ECM,posYIni_ECM ;
 vector <EType> eNodeVec ;  
-readCoord_ECM.open("./resources/coordinate_ECM17.txt") ;
+readCoord_ECM.open("./resources/coordinate_ECM18.txt") ;
 if (readCoord_ECM.is_open()) {
 	cout << "ECM coordinates file opened successfully" <<endl ; 
 }
